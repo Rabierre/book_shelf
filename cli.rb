5ad5d78751e043b280ad8a14c8a3617fe4c08457
rabierre <rabierre@gmail.com>
2013-07-04 16:16:26 +0900

    change tabs to spaces

--- cli.rb
+++ cli.rb
@@ -1,35 +1,35 @@
 require File.expand_path(File.dirname(__FILE__) + '/book_shelf.rb')
 
 class CLI
-	attr_accessor :bookshelf
+    attr_accessor :bookshelf
 
-	def initialize
-		@bookshelf = BookShelf.new
-		@bookshelf.loadShelf
-	end
+    def initialize
+        @bookshelf = BookShelf.new
+        @bookshelf.loadShelf
+    end
 
-	def fetch
-		print ">> "
+    def fetch
+        print ">> "
 
-		loop do |line = gets.chomp!|
-			case line
-			when "quit"
-				break
-			when "list"
-				@bookshelf.listBooks
-			when "add"
-				bookInfo = gets.chomp!.split(",").each { |info| info.strip! }
-				@bookshelf.addBook Book.new(bookInfo[0], bookInfo[1])
-			when "save"
-				@bookshelf.saveList
-			when "help"
-				puts "list, add, save"
-			else
-				puts "There's no such instruction [#{line}]!"
-			end
-			print ">>"
-		end
-	end
+        loop do |line = gets.chomp!|
+            case line
+            when "quit"
+                break
+            when "list"
+                @bookshelf.listBooks
+            when "add"
+                bookInfo = gets.chomp!.split(",").each { |info| info.strip! }
+                @bookshelf.addBook Book.new(bookInfo[0], bookInfo[1])
+            when "save"
+                @bookshelf.saveList
+            when "help"
+                puts "list, add, save"
+            else
+                puts "There's no such instruction [#{line}]!"
+            end
+            print ">>"
+        end
+    end
 end
 
 # Do CMD Process
